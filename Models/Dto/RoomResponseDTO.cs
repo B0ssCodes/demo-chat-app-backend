e0e1e1a364b56e69f72d84a28c3f8b27e9230411
Boss Codes <jadsaderkarate@gmail.com>
2024-07-20 11:02:02 +0300

    Added MessageCount to RoomResponseDTO

--- Models/Dto/RoomResponseDTO.cs
+++ Models/Dto/RoomResponseDTO.cs
@@ -5,6 +5,7 @@
         public int RoomId { get; set; }
         public string Name { get; set; }
         public string Description { get; set; }
+        public int MessageCount { get; set; }
         public List<UserDTO> Users { get; set; }
         //public List<MessageResponseDTO> Messages { get; set; }
     }
